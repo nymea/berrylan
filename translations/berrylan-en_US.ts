da976d9312ac1cb378b1af9cc5f2401cd5407e79
Michael Zanetti <michael_zanetti@gmx.net>
2018-10-15 14:52:56 +0200

    add base translation

---
+++ translations/berrylan-en_US.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>BerryLanPage</name>
+    <message>
+        <source>an open source tool by</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothDevice</name>
+    <message>
+        <source>Connecting to %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connected to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disconnecting from %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Discovering services of %1...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 connected and discovered.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 disconnected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <source>About BerryLan</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WirelessSetupManager</name>
+    <message>
+        <source>Invalid value.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There is no networkmanager available on the device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There is no wireless device available on the device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown error occured.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid command.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid parameters.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The wireless networking is disabled on the device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The networking is disabled on the device.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>BerryLan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sorry, the password is wrong.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Try again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bluetooth doesn&apos;t seem to be available on this device. BerryLan requires a working Bluetooth connection.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bluetooth seems to be disabled. Please enable Bluetooth on your device in order to use BerryLan.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Searching for your
+Raspberry Pi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connecting to your Raspberry Pi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connecting the Raspberry Pi to %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thanks for using BerryLan!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
