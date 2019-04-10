69e7928d50e07c29ba926e43268978893c3ea56a
Juan Biondi <juanernestobiondi@gmail.com>
2019-04-10 11:36:28 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: Open Source apps/BerryLan
    Translate-URL: http://translate.nymea.io/projects/open-source-apps/berrylan/es/

--- translations/berrylan-es.ts
+++ translations/berrylan-es.ts
@@ -66,15 +66,15 @@
     </message>
     <message>
         <source>Invalid parameters.</source>
-        <translation>Parámetros invalidos.</translation>
+        <translation>Parámetros inválidos.</translation>
     </message>
     <message>
         <source>The wireless networking is disabled on the device.</source>
-        <translation></translation>
+        <translation>Red inalambrica desabilitada en el dispositivo.</translation>
     </message>
     <message>
         <source>The networking is disabled on the device.</source>
-        <translation></translation>
+        <translation>La red no esta habilitada en el dispositivo.</translation>
     </message>
 </context>
 <context>
@@ -85,48 +85,49 @@
     </message>
     <message>
         <source>Sorry, the password is wrong.</source>
-        <translation></translation>
+        <translation>Disculpe, contraseña invalida.</translation>
     </message>
     <message>
         <source>Try again</source>
-        <translation></translation>
+        <translation>Inténtelo de nuevo</translation>
     </message>
     <message>
         <source>Devices</source>
-        <translation></translation>
+        <translation>Dispositivos</translation>
     </message>
     <message>
         <source>Network</source>
-        <translation></translation>
+        <translation>Red</translation>
     </message>
     <message>
         <source>Login</source>
-        <translation></translation>
+        <translation>Iniciar sesión</translation>
     </message>
     <message>
         <source>Connecting</source>
-        <translation></translation>
+        <translation>Conectando</translation>
     </message>
     <message>
         <source>Connected</source>
-        <translation></translation>
+        <translation>Conectado</translation>
     </message>
     <message>
         <source>Bluetooth doesn&apos;t seem to be available on this device. BerryLan requires a working Bluetooth connection.</source>
-        <translation></translation>
+        <translation>Bluetooth parece no esta disponible en este dispositivo. BerryLan requiere una conexión Bluetooth.</translation>
     </message>
     <message>
         <source>Bluetooth seems to be disabled. Please enable Bluetooth on your device in order to use BerryLan.</source>
-        <translation></translation>
+        <translation>Bluetooth parece no estar habilitado. Por favor active el Bluetooth en su dispositivo para usar BerryLan.</translation>
     </message>
     <message>
         <source>Searching for your
 Raspberry Pi</source>
-        <translation></translation>
+        <translation>Buscando
+Raspberry Pi</translation>
     </message>
     <message>
         <source>Connecting to your Raspberry Pi</source>
-        <translation></translation>
+        <translation>Conectandose a tu Raspberry Pi</translation>
     </message>
     <message>
         <source>OK</source>
