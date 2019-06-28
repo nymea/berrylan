dfd3677fd1caa0ab08dec170c822dc8d83920db3
Bernhard Trinnes <bernhard.trinnes@guh.io>
2019-06-28 07:15:08 +0000

    Translated using Weblate (French)
    Currently translated at 96.7% (30 of 31 strings)
    
    Translation: Open Source apps/BerryLan
    Translate-URL: http://translate.nymea.io/projects/open-source-apps/berrylan/fr/

--- translations/berrylan-fr.ts
+++ translations/berrylan-fr.ts
@@ -5,76 +5,76 @@
     <name>BerryLanPage</name>
     <message>
         <source>an open source tool by</source>
-        <translation></translation>
+        <translation type="unfinished">un programme accessible et modifiable</translation>
     </message>
 </context>
 <context>
     <name>BluetoothDevice</name>
     <message>
         <source>Connecting to %1...</source>
-        <translation></translation>
+        <translation>Connecte à %1...</translation>
     </message>
     <message>
         <source>Connected to %1</source>
-        <translation></translation>
+        <translation>Connecté à %1</translation>
     </message>
     <message>
         <source>Disconnecting from %1...</source>
-        <translation></translation>
+        <translation>Déconnecte de %1...</translation>
     </message>
     <message>
         <source>Discovering services of %1...</source>
-        <translation></translation>
+        <translation>Cherche service à %1...</translation>
     </message>
     <message>
         <source>%1 connected and discovered.</source>
-        <translation></translation>
+        <translation>%1 connecté et exploré.</translation>
     </message>
     <message>
         <source>%1 disconnected.</source>
-        <translation></translation>
+        <translation>%1 déconnecté.</translation>
     </message>
 </context>
 <context>
     <name>HelpPage</name>
     <message>
         <source>About BerryLan</source>
-        <translation></translation>
+        <translation>Via BerryLan</translation>
     </message>
 </context>
 <context>
     <name>WirelessSetupManager</name>
     <message>
         <source>Invalid value.</source>
-        <translation></translation>
+        <translation>Valeur non valable.</translation>
     </message>
     <message>
         <source>There is no networkmanager available on the device.</source>
-        <translation></translation>
+        <translation>La gestion de réseau n’est pas disponible sur cet appareil.</translation>
     </message>
     <message>
         <source>There is no wireless device available on the device.</source>
-        <translation></translation>
+        <translation>Le module de communication radio n’est pas disponible sur cet appareil.</translation>
     </message>
     <message>
         <source>Unknown error occured.</source>
-        <translation></translation>
+        <translation>Une erreur de cause s’est produite.</translation>
     </message>
     <message>
         <source>Invalid command.</source>
-        <translation></translation>
+        <translation>Commande non valable.</translation>
     </message>
     <message>
         <source>Invalid parameters.</source>
-        <translation></translation>
+        <translation>Paramètre non valable.</translation>
     </message>
     <message>
         <source>The wireless networking is disabled on the device.</source>
-        <translation></translation>
+        <translation>La communication radio est inactivée sur cet appareil.</translation>
     </message>
     <message>
         <source>The networking is disabled on the device.</source>
-        <translation></translation>
+        <translation>Le réseau est inactivé sur cet appareil.</translation>
     </message>
 </context>
 <context>
@@ -85,48 +85,49 @@
     </message>
     <message>
         <source>Sorry, the password is wrong.</source>
-        <translation></translation>
+        <translation>Le mot de passe est incorrect.</translation>
     </message>
     <message>
         <source>Try again</source>
-        <translation></translation>
+        <translation>S.v.p. essayer de nouveau</translation>
     </message>
     <message>
         <source>Devices</source>
-        <translation></translation>
+        <translation>Appareils</translation>
     </message>
     <message>
         <source>Network</source>
-        <translation></translation>
+        <translation>Reseau</translation>
     </message>
     <message>
         <source>Login</source>
-        <translation></translation>
+        <translation>Inscription</translation>
     </message>
     <message>
         <source>Connecting</source>
-        <translation></translation>
+        <translation>Connecte</translation>
     </message>
     <message>
         <source>Connected</source>
-        <translation></translation>
+        <translation>Connecté</translation>
     </message>
     <message>
         <source>Bluetooth doesn&apos;t seem to be available on this device. BerryLan requires a working Bluetooth connection.</source>
-        <translation></translation>
+        <translation>Bluetooth n’est pas disponible sur cet appareil. BerryLan a besoin d’une connection Bluetooth qui fonctionne.</translation>
     </message>
     <message>
         <source>Bluetooth seems to be disabled. Please enable Bluetooth on your device in order to use BerryLan.</source>
-        <translation></translation>
+        <translation>Bluetooth semble être inactivé. Pour utiliser BerryLan tu dois activer Bluetooth.</translation>
     </message>
     <message>
         <source>Searching for your
 Raspberry Pi</source>
-        <translation></translation>
+        <translation>Cherche ton 
+Raspberry Pi</translation>
     </message>
     <message>
         <source>Connecting to your Raspberry Pi</source>
-        <translation></translation>
+        <translation>Connecte à ton Raspberry Pi</translation>
     </message>
     <message>
         <source>OK</source>
@@ -134,11 +135,11 @@ Raspberry Pi</source>
     </message>
     <message>
         <source>Connecting the Raspberry Pi to %1</source>
-        <translation></translation>
+        <translation>Connecte le Raspberry Pi à %1</translation>
     </message>
     <message>
         <source>Thanks for using BerryLan!</source>
-        <translation></translation>
+        <translation>Merci beaucoup d’utiliser BerryLan!</translation>
     </message>
 </context>
 </TS>
