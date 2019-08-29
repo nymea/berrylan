c44d667fc3134e8d30597f5678b0e57258375728
DaeBong Jeon <windself3735@naver.com>
2019-08-29 09:36:06 +0000

    Added translation using Weblate (English)

---
+++ translations/berrylan-en.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>BerryLanPage</name>
+    <message>
+        <source>an open source tool by</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>BluetoothDevice</name>
+    <message>
+        <source>Connecting to %1...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connected to %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Disconnecting from %1...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Discovering services of %1...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 connected and discovered.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 disconnected.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>HelpPage</name>
+    <message>
+        <source>About BerryLan</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>WirelessSetupManager</name>
+    <message>
+        <source>Invalid value.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>There is no networkmanager available on the device.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>There is no wireless device available on the device.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Unknown error occured.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Invalid command.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Invalid parameters.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>The wireless networking is disabled on the device.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>The networking is disabled on the device.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>BerryLan</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Sorry, the password is wrong.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Try again</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Devices</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Network</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connecting</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Bluetooth doesn&apos;t seem to be available on this device. BerryLan requires a working Bluetooth connection.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Bluetooth seems to be disabled. Please enable Bluetooth on your device in order to use BerryLan.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Searching for your
+Raspberry Pi</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connecting to your Raspberry Pi</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Connecting the Raspberry Pi to %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Thanks for using BerryLan!</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
